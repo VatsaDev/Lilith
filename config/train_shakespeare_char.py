67c7791ecfc8f6d4215edea25163d33b40a41f9a
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-02-18 21:45:28 -0600

    Update train_shakespeare_char.py

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -24,10 +24,10 @@ n_head = 6
 n_embd = 384
 dropout = 0.2
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+learning_rate = 1e-2 # with baby networks can afford to go a bit higher
 max_iters = 5000
 lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
+min_lr = 1e-3 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
