09530a94bf1ca3167920a9e924491d87e5621c14
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-02-19 01:17:59 -0600

    Update train.py

--- train.py
+++ train.py
@@ -468,7 +468,7 @@ while True:
         if local_iter_num >= 5: # let the training loop settle a bit
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
-        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, lr:{lr} , mfu: {running_mfu*100:.2f}%")
         losses_track.append(lossf)
         print(losses_track)
     iter_num += 1
