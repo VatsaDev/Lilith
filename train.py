e2cae6ea8d1df898c145bd0f9cdcff035d969e41
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-03-10 20:05:29 -0500

    Update train.py

--- train.py
+++ train.py
@@ -94,8 +94,8 @@ class Lilith(Optimizer):
         weight_decay: float = 0.01,
         g_norm_min: float = 1e-4,
         ratio_min: float = 1e-4,
-        acceleration: float = 1,
-        ema_k: int = 0,
+        acceleration: float = 4,
+        ema_k: int = 0.01*max_iters,
         ema_beta: float = 0.99
     ):
         defaults = dict(
@@ -107,7 +107,7 @@ class Lilith(Optimizer):
             weight_decay=weight_decay,
             g_norm_min=g_norm_min,
             ratio_min=ratio_min,
-            acceleration=acceleration,
+            acceleration=acc,
             ema_k = ema_k,
             ema_beta=ema_beta,
         )
